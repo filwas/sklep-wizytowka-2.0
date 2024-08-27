b13c5c4b5d882ee4f29181ebf454d568ca2bbebe
filwas <fwas.corp@gmail.com>
2024-08-27 18:53:56 +0200

    sanity schema

--- src/sanity/schemaTypes/itemType.ts
+++ src/sanity/schemaTypes/itemType.ts
@@ -6,13 +6,31 @@ export const itemType = defineType({
   type: "document",
   fields: [
     defineField({
+      title: "Name",
       name: "name",
       type: "string",
+      validation: rule => rule.required()
     }),
     defineField({
+      title: "Category",
       name: "category",
       type: "reference",
-      to: [{type: 'category'}]
+      to: [{ type: "category" }],
+      validation: rule => rule.required(),
     }),
+    defineField({
+      title: "Images",
+      name: "image",
+      type: "array",
+      of: [{ type: "image" }],
+      options: { layout: "grid" },
+      validation: rule => rule.required()
+    }),
+    defineField({
+      title: 'Text', 
+      name: 'content',
+      type: 'array', 
+      of: [{type: 'block'}]
+    })
   ],
 });
